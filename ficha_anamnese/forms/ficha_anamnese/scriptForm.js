7fba12166c9456142117080c1dc6f42235dcd736
Matheus Aurelio <matheusaureliodeo4@gmail.com>
2025-04-28 20:37:09 -0300

    realizada interação via API viaCEP para o preenchimento e colapse de todo do conteudo da ficha anamnese

--- ficha_anamnese/forms/ficha_anamnese/scriptForm.js
+++ ficha_anamnese/forms/ficha_anamnese/scriptForm.js
@@ -12,3 +12,25 @@ function toggleCollapse(id) {
 var content = document.getElementById(id);
 content.classList.toggle('active');
 }
+
+function consultaCep() {
+  var cep = document.getElementById('cep').value.replace(/\D/g, ''); 
+  // pega o valor (.value) digitado no input cep.
+  // (replace(/\D/g, '')) remove tudo que não for número 
+
+  var url = "https://viacep.com.br/ws/"+cep+"/json"; //armazena o conteudo do site/api viacep
+    $.ajax({ // chamada AJAX
+      url: url, // acessa a URL da API
+      type: "get", // usa tipo GET (obter)
+      dataType: "json", // tipo json
+      success: function(dados){
+        console.log(dados)
+        document.getElementById("bairro").value = dados.bairro
+        document.getElementById("logradouro").value = dados.logradouro
+      }
+    })
+}
+
+// Função consultaCep, serve para autopreenchimento,
+// quando o usuario preencher o cep no input os campos bairro
+//  e logradoura será preenchido através da API ViaCEP.
